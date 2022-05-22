37ae2ba100b9567436abc4e65b9efabaf187bd90
Nahid-ul-Islam <nahidul522@gmail.com>
2022-05-22 22:27:27 +0600

    created booking by id route

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 //jwt
 const jwt = require('jsonwebtoken');
@@ -146,7 +146,14 @@ async function run() {
                 return res.status(403).send({ message: 'forbidden access' });
             }
 
-        })
+        });
+
+        app.get('/booking/:id', verifyJWT, async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const booking = await bookingCollection.findOne(query);
+            res.send(booking);
+        });
 
 
         app.post('/booking', async (req, res) => {
